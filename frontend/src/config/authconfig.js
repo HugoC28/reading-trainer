32ae75027497c854bd37ad28445e2a7c0c5da023
Janne <janne.lahdentausta25@gmail.com>
2023-11-21 22:08:46 +0100

    Small fix

--- frontend/src/config/authconfig.js
+++ frontend/src/config/authconfig.js
@@ -10,7 +10,6 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-console.log("firebaseConfig", firebaseConfig);
 const firebaseApp = initializeApp(firebaseConfig);
 
 export default firebaseApp;
